b8e2041c1e66535c8035fb237c175ce683db037b
Aponia <brianmv@uci.edu>
2023-09-02 10:58:31 -0700

    feat: ✨ get proxy form state

---
+++ packages/core/src/logic/getProxyFormState.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { VALIDATION_MODE } from '../constants'
+
+/**
+ * But this isn't "getting" anything, it's creating a new object?
+ */
+export function getProxyFormState(
+  formState: any,
+  control: any,
+  localProxyFormState?: any,
+  isRoot = true,
+) {
+  const result = { defaultValues: control._defaultValues }
+
+  for (const key in formState) {
+    Object.defineProperty(result, key, {
+      get: () => {
+        const _key = key
+
+        if (control._proxyFormState[_key] !== VALIDATION_MODE.all) {
+          control._proxyFormState[_key] = !isRoot || VALIDATION_MODE.all
+        }
+
+        if (localProxyFormState) {
+          localProxyFormState[_key] = true
+        }
+
+        return formState[_key]
+      },
+    })
+  }
+
+  return result
+}
