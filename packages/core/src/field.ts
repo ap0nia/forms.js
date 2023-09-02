75cc83b8537e6a61ae420dd27bfb3f23f2a991ba
Aponia <brianmv@uci.edu>
2023-09-02 12:52:08 -0700

    feat: ✨ field

---
+++ packages/core/src/field.ts
@@ -0,0 +1,80 @@
+import type { FlattenObject } from './utils/flatten-object'
+import type { Noop } from './utils/noop'
+
+/**
+ * A custom field element has these properties.
+ */
+export type CustomElement<T extends Record<PropertyKey, unknown> = Record<PropertyKey, unknown>> = {
+  /**
+   * The name of the field.
+   */
+  name: keyof FlattenObject<T>
+
+  /**
+   * The type of the field.
+   *
+   * @example 'checkbox', 'radio', 'select', 'file'
+   */
+  type?: string
+
+  /**
+   * The value of the field.
+   */
+  value?: unknown
+
+  /**
+   * Whether the field is disabled.
+   */
+  disabled?: boolean
+
+  /**
+   * Whether the field is checked. i.e. for checkboxes and radio buttons.
+   */
+  checked?: boolean
+
+  /**
+   * Options for the field. i.e. for select elements.
+   */
+  options?: HTMLOptionsCollection
+
+  /**
+   * The files of the field. i.e. for file inputs.
+   */
+  files?: FileList | null
+
+  /**
+   * What to do when the field is focused.
+   */
+  focus?: Noop
+}
+
+export type FieldElement<T extends Record<PropertyKey, unknown> = Record<PropertyKey, unknown>> =
+  | HTMLInputElement
+  | HTMLSelectElement
+  | HTMLTextAreaElement
+  | CustomElement<T>
+
+/**
+ * Idk what this stands for.
+ */
+export type Field = {
+  /**
+   * The original element referenced by the field.
+   */
+  ref: FieldElement
+
+  /**
+   * The name of the field.
+   */
+  name: string
+
+  /**
+   * Actual HTML elements used by the field.
+   */
+  refs?: HTMLInputElement[]
+
+  /**
+   * Whether it's mounted?
+   */
+  mount?: boolean
+} & RegisterOptions
