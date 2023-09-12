a4f2959eb27bb2e510de867f601d90823215ea82
Aponia <brianmv@uci.edu>
2023-09-12 08:08:23 -0700

    feat: ✨ more validation logic for min/max

--- packages/core/src/logic/validation.ts
+++ packages/core/src/logic/validation.ts
@@ -1,7 +1,9 @@
+import { INPUT_VALIDATION_RULES } from '../constants'
 import { getCheckboxValue, isCheckBoxInput } from '../utils/html/checkbox'
 import { isFileInput } from '../utils/html/file'
 import { isHTMLElement } from '../utils/html/is-html-element'
 import { getRadioValue, isRadioInput } from '../utils/html/radio'
+import { isObject } from '../utils/is-object'
 import { notNullish } from '../utils/null'
 import { safeGet } from '../utils/safe-get'
 
@@ -17,7 +19,7 @@ export type ValidationValue = boolean | number | string | RegExp
  * Maybe this is referenced when displaying validation errors?
  */
 export type ValidationValueMessage<T = ValidationValue> = {
-  value: T
+  value?: T
   message: string
 }
 
@@ -123,20 +125,107 @@ async function nativelyValidateField(
     return {}
   }
 
-  const inputValue = safeGet(formValues, field._f.name)
+  const { ref, refs, name } = field._f
 
-  const inputRef = (field._f.refs ? field._f.refs[0] : field._f.ref) as HTMLInputElement
+  const inputValue = safeGet(formValues, name)
+
+  const inputRef = (refs ? refs[0] : ref) as HTMLInputElement
 
   const shouldSetCustomValidity = shouldUseNativeValidation && inputRef.reportValidity
 
   const errors: InternalFieldErrors = {}
 
-  hasError(field, inputValue, isFieldArray)
+  if (requiredButMissing(field, inputValue, isFieldArray)) {
+    const { value, message } = getValueAndMessage(field._f.required)
 
-  validateAllFieldCriteria
+    if (value) {
+      errors[name] = {
+        type: INPUT_VALIDATION_RULES.required,
+        message,
+        ref: inputRef,
+        ...(validateAllFieldCriteria && {
+          ...errors[name],
+          types: {
+            ...errors[name]?.types,
+            [INPUT_VALIDATION_RULES.required]: message || true,
+          },
+        }),
+      }
 
-  if (shouldSetCustomValidity) {
-    setCustomValidity(inputRef, '')
+      if (!validateAllFieldCriteria && shouldSetCustomValidity) {
+        setCustomValidity(inputRef, message)
+        return errors
+      }
+    }
+  }
+
+  const isEmpty = refIsEmpty(field, inputValue)
+
+  const { min, max } = field._f
+
+  if (!isEmpty && (min != null || max != null)) {
+    let exceedMax
+    let exceedMin
+    const maxOutput = getValueAndMessage(max)
+    const minOutput = getValueAndMessage(min)
+
+    if (inputValue != null && !isNaN(inputValue)) {
+      const valueNumber =
+        (ref as HTMLInputElement).valueAsNumber || (inputValue ? +inputValue : inputValue)
+
+      if (maxOutput.value != null) {
+        exceedMax = valueNumber > maxOutput.value
+      }
+
+      if (minOutput.value != null) {
+        exceedMin = valueNumber < minOutput.value
+      }
+    } else {
+      const valueDate = (ref as HTMLInputElement).valueAsDate || new Date(inputValue as string)
+      const convertTimeToDate = (time: unknown) => new Date(new Date().toDateString() + ' ' + time)
+      const isTime = ref.type == 'time'
+      const isWeek = ref.type == 'week'
+
+      if (typeof maxOutput.value === 'string' && inputValue) {
+        exceedMax = isTime
+          ? convertTimeToDate(inputValue) > convertTimeToDate(maxOutput.value)
+          : isWeek
+          ? inputValue > maxOutput.value
+          : valueDate > new Date(maxOutput.value)
+      }
+
+      if (typeof minOutput.value === 'string' && inputValue) {
+        exceedMin = isTime
+          ? convertTimeToDate(inputValue) < convertTimeToDate(minOutput.value)
+          : isWeek
+          ? inputValue < minOutput.value
+          : valueDate < new Date(minOutput.value)
+      }
+    }
+
+    if (exceedMax || exceedMin) {
+      const message = exceedMax ? maxOutput.message : minOutput.message
+
+      const validationType = exceedMax ? INPUT_VALIDATION_RULES.max : INPUT_VALIDATION_RULES.min
+
+      errors[name] = {
+        type: validationType,
+        message,
+        ref,
+        ...(validateAllFieldCriteria && {
+          ...errors[name],
+          types: {
+            ...errors[name]?.types,
+            [validationType]: message || true,
+          },
+        }),
+      }
+
+      if (!validateAllFieldCriteria && shouldSetCustomValidity) {
+        setCustomValidity(inputRef, errors[name]?.message)
+        return errors
+      }
+    }
   }
 
   return errors
@@ -147,11 +236,13 @@ export function setCustomValidity(inputRef: HTMLInputElement, message?: string |
   inputRef.reportValidity()
 }
 
-function hasError(field: Field, inputValue: any, isFieldArray?: boolean) {
-  if ((isFieldArray && !Array.isArray(inputValue)) || !inputValue.length) {
+function requiredButMissing(field: Field, inputValue: any, isFieldArray?: boolean) {
+  // Invalid field array.
+  if (isFieldArray && (!Array.isArray(inputValue) || !inputValue.length)) {
     return true
   }
 
+  // If the field is not required, then it isn't missing.
   if (!field._f.required) {
     return false
   }
@@ -161,18 +252,22 @@ function hasError(field: Field, inputValue: any, isFieldArray?: boolean) {
   const isRadioOrCheckbox = isRadio || isCheckBox
   const isEmpty = refIsEmpty(field, inputValue)
 
+  // If it's **not** a radio or checkbox and there's no value, then it's missing.
   if (!isRadioOrCheckbox && (isEmpty || inputValue == null)) {
     return true
   }
 
-  if (typeof inputValue === 'boolean' && !inputValue) {
+  // If the input value is false, it's missing?
+  if (inputValue === false) {
     return true
   }
 
+  // Invalid checkbox value.
   if (isCheckBox && !getCheckboxValue(field._f.refs).isValid) {
     return true
   }
 
+  // Invalid radio value.
   if (isRadio && !getRadioValue(field._f.refs).isValid) {
     return true
   }
@@ -192,4 +287,16 @@ function refIsEmpty(field: Field, inputValue: unknown) {
   }
 
   return Array.isArray(inputValue) && !inputValue.length
+}
+
+export function getValueAndMessage(validationRule?: ValidationRule): ValidationValueMessage {
+  if (typeof validationRule === 'string') {
+    return { value: Boolean(validationRule), message: validationRule }
+  }
+
+  if (isObject(validationRule) && !(validationRule instanceof RegExp)) {
+    return validationRule
+  }
+
+  return { value: validationRule, message: '' }
 }
