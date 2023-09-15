16bea4340621067739d4be0c2981d3932286c319
Aponia <brianmv@uci.edu>
2023-09-14 20:33:30 -0700

    test: 🚨 full coverage for fields

--- packages/core/tests/logic/fields.test.ts
+++ packages/core/tests/logic/fields.test.ts
@@ -1,6 +1,6 @@
 import { vi, describe, it, expect } from 'vitest'
 
-import { getFieldValue, type Field } from '../../src/logic/fields'
+import { getFieldValue, getFieldValueAs, type Field } from '../../src/logic/fields'
 
 vi.mock('../../src/utils/html/radio', async (importOriginal) => {
   const mod = (await importOriginal()) ?? {}
@@ -96,5 +96,115 @@ describe('getFieldValue', () => {
         },
       }),
     ).toEqual(undefined)
+  })
+
+  it('return multiple select input selected options', () => {
+    expect(
+      getFieldValue({
+        name: 'test',
+        ref: {
+          name: 'test',
+          type: 'select-multiple',
+          selectedOptions: [{ value: 'test' }] as any,
+        },
+      }),
+    ).toEqual(['test'])
+  })
+
+  it('returns nullish value if refs are disabled', () => {
+    expect(
+      getFieldValue({
+        name: 'test',
+        ref: {
+          name: 'test',
+          disabled: true,
+        },
+        refs: [
+          {
+            name: 'test',
+            disabled: true,
+          },
+        ] as any,
+      }),
+    ).toEqual(undefined)
+  })
+})
+
+describe('getFieldValueAs', () => {
+  it('should return undefined when value is undefined', () => {
+    expect(
+      getFieldValueAs(undefined, {
+        ref: {
+          name: 'test',
+        },
+        name: 'test',
+        valueAsNumber: true,
+        valueAsDate: false,
+      }),
+    ).toBeUndefined()
+  })
+
+  it('should return a date object if the value is a string and date was specified', () => {
+    expect(
+      getFieldValueAs('2020-01-01', {
+        ref: {
+          name: 'test',
+        },
+        name: 'test',
+        valueAsDate: true,
+      }),
+    ).toEqual(new Date('2020-01-01'))
+  })
+
+  it('should use a custom set value as function', () => {
+    const value = '2020-01-01'
+
+    const setValueAs = (value: string) => value + 'aponia'
+
+    expect(
+      getFieldValueAs(value, {
+        ref: {
+          name: 'test',
+        },
+        name: 'test',
+        setValueAs,
+      }),
+    ).toEqual(setValueAs(value))
+  })
+
+  it('returns nan if value as number specified but empty string', () => {
+    expect(
+      getFieldValueAs('', {
+        ref: {
+          name: 'test',
+        },
+        name: 'test',
+        valueAsNumber: true,
+      }),
+    ).toEqual(NaN)
+  })
+
+  it('returns the same value if value as number specified but value nullish', () => {
+    expect(
+      getFieldValueAs(0, {
+        ref: {
+          name: 'test',
+        },
+        name: 'test',
+        valueAsNumber: true,
+      }),
+    ).toEqual(0)
+  })
+
+  it('returns a string converted to number if value as number specified', () => {
+    expect(
+      getFieldValueAs('1', {
+        ref: {
+          name: 'test',
+        },
+        name: 'test',
+        valueAsNumber: true,
+      }),
+    ).toEqual(1)
   })
 })
