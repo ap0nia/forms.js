2fcb9b22a8e9e342777e6bab00cd40b302f8c88e
Aponia <brianmv@uci.edu>
2023-09-16 16:12:48 -0700

    feat: ✨ clone-object utility

---
+++ packages/core/src/utils/clone-object.ts
@@ -0,0 +1,36 @@
+import { isBrowser } from './is-browser'
+import { isObject, isPlainObject } from './is-object'
+
+export function cloneObject<T>(data: T): T {
+  if (data instanceof Date) {
+    return new Date(data) as T
+  }
+
+  if (data instanceof Set) {
+    return new Set(data) as T
+  }
+
+  const dataIsArray = Array.isArray(data)
+
+  if (!(isBrowser() && isRawData(data)) && (dataIsArray || isObject(data))) {
+    if (!dataIsArray && !isPlainObject(data)) {
+      return data
+    }
+
+    const copy: any = dataIsArray ? [] : {}
+
+    for (const key in data) {
+      if (Object.prototype.hasOwnProperty.call(data, key)) {
+        copy[key] = cloneObject(data[key])
+      }
+    }
+
+    return copy
+  }
+
+  return data
+}
+
+function isRawData(value: unknown): value is Blob | FileList {
+  return value instanceof Blob || value instanceof FileList
+}
