e20f418a4113ade6ed16c4ed0b1c2cb76e0aa565
Aponia <brianmv@uci.edu>
2023-09-15 23:29:12 -0700

    refactor: ♻️ distinguish between top-level and nested explicit any

--- packages/core/src/utils/types/deep-map.ts
+++ packages/core/src/utils/types/deep-map.ts
@@ -3,7 +3,10 @@ import type { IsAny } from './is-any'
 /**
  * Maps all of an object's properties and sub-properties to a new type.
  *
- * Does not handle explicit `any`.
+ * At the top level, explicit `any` is preserved as `any`.
+ * Below the top level, properties explicitly typed as `any` are mapped to the new type.
+ *
+ * A third generic param is used to represent this, lol.
  *
  * @example
  *
@@ -21,8 +24,10 @@ import type { IsAny } from './is-any'
  * type DeepMapNumberMyType = DeepMap<MyType, number>
  *      // ^? type DeepMapNumberMyType = { a: number; b: { c: number; d: { e: number;  } } }
  */
-export type DeepMap<T, TType> = IsAny<T> extends true
-  ? any
+export type DeepMap<T, TType, IsInner extends boolean = false> = IsAny<T> extends true
+  ? IsInner extends true
+    ? TType
+    : any
   : T extends Record<PropertyKey, any>
-  ? { [K in keyof T]: T[K] extends undefined ? never : DeepMap<T[K], TType> }
+  ? { [K in keyof T]: DeepMap<T[K], TType, true> }
   : TType
