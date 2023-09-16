c77d2b9570bba20d83c7f7e6e4df68cc18800449
Aponia <brianmv@uci.edu>
2023-09-15 23:04:32 -0700

    refactor: ♻️ documentation for prettify

--- packages/core/src/utils/types/prettify.ts
+++ packages/core/src/utils/types/prettify.ts
@@ -6,7 +6,25 @@ import type { IsAny } from './is-any'
  * @see https://twitter.com/mattpocockuk/status/1622730173446557697?s=20
  *
  * Explicit `any` will not be handled.
+ *
+ * The main purpose is to merge an intersection of objects into a single object.
+ *
+ * @example
+ *
+ * ```ts
+ * type A = { a: string }
+ * type B = { b: number }
+ * type C = { c: boolean }
+ *
+ * // This is one object with all the properties in A, B, and C. But it's hard to read.
+ * type ABC = A & B & C
+ *
+ * // Much better!
+ * type Prettified = Prettify<ABC>
+ *      // ^? type Prettified = { a: string; b: number; c: boolean }
+ *
+ * ```
  */
 export type Prettify<T extends Record<PropertyKey, any>> = IsAny<T> extends true
-  ? T
+  ? any
   : { [K in keyof T]: T[K] } & {}
