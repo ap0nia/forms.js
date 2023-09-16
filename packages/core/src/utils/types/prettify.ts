1d727445cb8bd34cfa58be5d7fa19e8fbc78ae5e
Aponia <brianmv@uci.edu>
2023-09-15 22:59:04 -0700

    refactor: ♻️ prettify don't handle explicity any

--- packages/core/src/utils/types/prettify.ts
+++ packages/core/src/utils/types/prettify.ts
@@ -1,8 +1,12 @@
 /* eslint-disable @typescript-eslint/ban-types */
 
+import type { IsAny } from './is-any'
+
 /**
  * @see https://twitter.com/mattpocockuk/status/1622730173446557697?s=20
+ *
+ * Explicit `any` will not be handled.
  */
-export type Prettify<T> = {
-  [K in keyof T]: T[K]
-} & {}
+export type Prettify<T extends Record<PropertyKey, any>> = IsAny<T> extends true
+  ? T
+  : { [K in keyof T]: T[K] } & {}
