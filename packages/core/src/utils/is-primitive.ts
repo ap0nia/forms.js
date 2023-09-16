dd0738881e3d2076813a949f59b2c0cbc49a084c
Aponia <brianmv@uci.edu>
2023-09-16 16:15:48 -0700

    feat: ✨ is-primitive utility

---
+++ packages/core/src/utils/is-primitive.ts
@@ -0,0 +1,5 @@
+export type Primitive = null | undefined | string | number | boolean | symbol | bigint
+
+export function isPrimitive(value: unknown): value is Primitive {
+  return value == null || typeof value !== 'object'
+}
