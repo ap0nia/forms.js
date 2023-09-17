6425289ada53174a315db93b8ffc4c48a9300ddd
Aponia <brianmv@uci.edu>
2023-09-17 06:40:23 -0700

    test: 🚨 is-html-element logic

---
+++ packages/core/tests/logic/html/is-html-element.client.ts
@@ -0,0 +1,30 @@
+/**
+ * @vitest-environment jsdom
+ */
+
+import { describe, test, expect } from 'vitest'
+
+import { isHTMLElement } from '../../../src/logic/html/is-html-element'
+
+/**
+ * @see https://github.com/react-hook-form/react-hook-form/blob/master/src/__tests__/utils/isHTMLElement.test.ts
+ */
+describe('isHTMLElement', () => {
+  test('returns true for HTMLElement', () => {
+    expect(isHTMLElement(document.createElement('input'))).toBeTruthy()
+  })
+
+  test('return trues for HTMLElement inside an iframe', () => {
+    const iframe = document.createElement('iframe')
+
+    document.body.append(iframe)
+
+    const iframeDocument = iframe.contentDocument!
+
+    const input = iframeDocument.createElement('input')
+
+    iframeDocument.body.append(input)
+
+    expect(isHTMLElement(input)).toBeTruthy()
+  })
+})
