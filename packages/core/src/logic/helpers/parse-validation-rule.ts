0a593b7ac055c521796db0cadc3406bd3e3aae69
Aponia <brianmv@uci.edu>
2023-09-17 16:36:38 -0700

    test: 🚨 parse-validation-rule

--- packages/core/src/logic/helpers/parse-validation-rule.ts
+++ packages/core/src/logic/helpers/parse-validation-rule.ts
@@ -4,7 +4,7 @@ import type { ValidationRule, ValidationValueMessage } from '../validation'
 /**
  * Helper function to get the value and message from a validation rule.
  */
-export function getValueAndMessage(validationRule?: ValidationRule): ValidationValueMessage {
+export function parseValidationRule(validationRule?: ValidationRule): ValidationValueMessage {
   return isObject(validationRule) && !(validationRule instanceof RegExp)
     ? validationRule
     : { value: validationRule, message: '' }
