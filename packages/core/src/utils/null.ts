ee160e5ff061661a0619852498a07a36f17b80b5
Aponia <brianmv@uci.edu>
2023-09-16 17:05:19 -0700

    feat: ✨ nullish utilities

---
+++ packages/core/src/utils/null.ts
@@ -0,0 +1,18 @@
+/**
+ * A nullish type. Because void isn't considered the same as null | undefined ??
+ */
+export type Nullish = null | undefined | void
+
+/**
+ * Not very useful.
+ */
+export function isNullish(value: unknown): value is Nullish {
+  return value == null
+}
+
+/**
+ * This is a necessary type guard mimic {@link Array.filter(Boolean)}.
+ */
+export function notNullish<T>(value: T): value is NonNullable<T> {
+  return !isNullish(value)
+}
