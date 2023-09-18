51fa52ec8f973fcbf5ed5523d62ff9e33db7a922
Aponia <brianmv@uci.edu>
2023-09-17 21:27:35 -0700

    feat: ✨ field-is-empty logic helper

---
+++ packages/core/src/logic/helpers/field-is-empty.ts
@@ -0,0 +1,20 @@
+import type { Field } from '../fields'
+import { isFileInput } from '../html/file'
+import { isHTMLElement } from '../html/is-html-element'
+
+/**
+ * Helper function describing whether a field's ref is empty.
+ */
+export function fieldRefIsEmpty(field: Field, inputValue: unknown): boolean {
+  const { ref, value, valueAsNumber } = field._f
+
+  if ((valueAsNumber || isFileInput(field._f.ref)) && value == null && inputValue == null) {
+    return true
+  }
+
+  if ((isHTMLElement(ref) && value === '') || inputValue === '') {
+    return true
+  }
+
+  return Array.isArray(inputValue) && !inputValue.length
+}
