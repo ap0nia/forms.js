5f63a11f3f4e01ff7cd3613e986696faed2a066d
Aponia <brianmv@uci.edu>
2023-09-26 10:15:52 -0700

    test(form-control): 🚨 update-valid

--- packages/core/tests/form-control/update-valid.test.ts
+++ packages/core/tests/form-control/update-valid.test.ts
@@ -1,6 +1,7 @@
-import { describe, test, expect } from 'vitest'
+import { describe, test, expect, vi } from 'vitest'
 
 import { FormControl } from '../../src/form-control'
+import { noop } from '../../src/utils/noop'
 
 describe('FormControl', () => {
   describe('update valid', () => {
@@ -14,6 +15,42 @@ describe('FormControl', () => {
       control.updateValid()
 
       expect(control.state.isValid.value).toBe(originalValue)
+    })
+
+    test('updates valid state for invalid form when subscribers present', async () => {
+      const control = new FormControl()
+
+      control.register('hello', { required: true })
+
+      const subscriber = vi.fn(noop)
+
+      control.state.isValid.subscribe(subscriber)
+
+      await control.updateValid()
+
+      // Since the value stayed false, it doesn't get notified again.
+      expect(subscriber).toHaveBeenCalledTimes(1)
+      expect(subscriber).toHaveBeenCalledWith(false)
+
+      expect(control.state.isValid.value).toBeFalsy()
+    })
+
+    test('updates valid state for valid form when subscribers present', async () => {
+      const control = new FormControl()
+
+      control.register('hello')
+
+      const subscriber = vi.fn(noop)
+
+      control.state.isValid.subscribe(subscriber)
+
+      await control.updateValid()
+
+      // The value changed from false to true, so it gets notified.
+      expect(subscriber).toHaveBeenCalledTimes(2)
+      expect(subscriber).toHaveBeenCalledWith(true)
+
+      expect(control.state.isValid.value).toBeTruthy()
     })
   })
 })
