9869dae2503e691de6173e1483360b16714348b8
Aponia <brianmv@uci.edu>
2023-09-27 15:36:33 -0700

    test(type util): keys-to-properties

---
+++ packages/core/tests/utils/types/keys-to-properties.test-d.ts
@@ -0,0 +1,37 @@
+import { describe, test, expectTypeOf } from 'vitest'
+
+import type { KeysToProperties } from '../../../src/utils/types/keys-to-properties'
+
+describe('KeysToProperties', () => {
+  describe('returns tuple of values for valid keys', () => {
+    test('simple object', () => {
+      type Foo = {
+        a: string
+        b: number
+        c: boolean
+      }
+
+      type Keys = ['a', 'b', 'c']
+
+      type Result = KeysToProperties<Foo, Keys>
+
+      expectTypeOf<Result>().toEqualTypeOf<[string, number, boolean]>()
+    })
+  })
+
+  describe('returns never for invalid keys', () => {
+    test('simple object', () => {
+      type Foo = {
+        a: string
+        b: number
+        c: boolean
+      }
+
+      type Keys = ['d', 'b', 'f']
+
+      type Result = KeysToProperties<Foo, Keys>
+
+      expectTypeOf<Result>().toEqualTypeOf<[never, number, never]>()
+    })
+  })
+})
