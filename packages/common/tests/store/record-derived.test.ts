828c629846786cd84129c64f42baabeded475a20
Aponia <brianmv@uci.edu>
2023-10-09 11:45:31 -0700

    refactor: ♻️ use create-derived

--- packages/common/tests/store/record-derived.test.ts
+++ packages/common/tests/store/record-derived.test.ts
@@ -2,132 +2,124 @@ import { describe, test, expect, vi } from 'vitest'
 
 import { Writable, RecordDerived } from '../../src/store'
 
+function createDerived(set?: Set<string>) {
+  const stores = {
+    a: new Writable(1),
+    b: new Writable(2),
+    c: new Writable(3),
+  }
+
+  const derived = new RecordDerived(stores, set)
+
+  return { stores, derived }
+}
+
 describe('store', () => {
   describe('RecordDerived', () => {
     describe('with keys set', () => {
       test('subscribes to all changes if no keys specified', () => {
-        const a = new Writable(1)
-        const b = new Writable(2)
-        const c = new Writable(3)
+        const { derived, stores } = createDerived()
 
         const fn = vi.fn()
 
-        const derived = new RecordDerived({ a, b, c })
-
         derived.subscribe(fn)
 
-        a.set(4)
+        stores.a.set(4)
 
         expect(fn).toHaveBeenLastCalledWith({ a: 4, b: 2, c: 3 })
 
-        b.set(5)
+        stores.b.set(5)
 
         expect(fn).toHaveBeenLastCalledWith({ a: 4, b: 5, c: 3 })
 
-        c.set(6)
+        stores.c.set(6)
 
         expect(fn).toHaveBeenLastCalledWith({ a: 4, b: 5, c: 6 })
       })
 
       test('subscribes to all changes if all keys included', () => {
-        const a = new Writable(1)
-        const b = new Writable(2)
-        const c = new Writable(3)
-
         const fn = vi.fn()
 
-        const derived = new RecordDerived({ a, b, c }, new Set(['a', 'b', 'c']))
+        const { derived, stores } = createDerived()
 
         derived.subscribe(fn)
 
-        a.set(4)
+        stores.a.set(4)
 
         expect(fn).toHaveBeenLastCalledWith({ a: 4, b: 2, c: 3 })
 
-        b.set(5)
+        stores.b.set(5)
 
         expect(fn).toHaveBeenLastCalledWith({ a: 4, b: 5, c: 3 })
 
-        c.set(6)
+        stores.c.set(6)
 
         expect(fn).toHaveBeenLastCalledWith({ a: 4, b: 5, c: 6 })
       })
 
       test('subscribes to specified keys', () => {
-        const a = new Writable(1)
-        const b = new Writable(2)
-        const c = new Writable(3)
-
         const fn = vi.fn()
 
-        const derived = new RecordDerived({ a, b, c }, new Set(['a']))
+        const { derived, stores } = createDerived(new Set(['a']))
 
         derived.subscribe(fn)
 
-        a.set(4)
+        stores.a.set(4)
 
         expect(fn).toHaveBeenLastCalledWith({ a: 4, b: 2, c: 3 })
 
         fn.mockReset()
 
-        b.set(5)
+        stores.b.set(5)
 
         expect(fn).not.toHaveBeenCalled()
 
-        c.set(6)
+        stores.c.set(6)
 
         expect(fn).not.toHaveBeenCalled()
       })
 
       test('never subscribes if empty set of keys specified', () => {
-        const a = new Writable(1)
-        const b = new Writable(2)
-        const c = new Writable(3)
-
         const fn = vi.fn()
 
-        const derived = new RecordDerived({ a, b, c }, new Set())
+        const { derived, stores } = createDerived(new Set())
 
         derived.subscribe(fn)
 
         fn.mockReset()
 
-        a.set(4)
+        stores.a.set(4)
 
         expect(fn).not.toHaveBeenCalled()
 
-        b.set(5)
+        stores.b.set(5)
 
         expect(fn).not.toHaveBeenCalled()
 
-        c.set(6)
+        stores.c.set(6)
 
         expect(fn).not.toHaveBeenCalled()
       })
 
       test('adapts if set of keys changes', () => {
-        const a = new Writable(1)
-        const b = new Writable(2)
-        const c = new Writable(3)
-
         const fn = vi.fn()
 
-        const derived = new RecordDerived({ a, b, c }, new Set())
+        const { derived, stores } = createDerived(new Set())
 
         derived.subscribe(fn)
 
         fn.mockReset()
 
-        a.set(4)
+        stores.a.set(4)
 
         expect(fn).not.toHaveBeenCalled()
 
-        b.set(5)
+        stores.b.set(5)
 
         expect(fn).not.toHaveBeenCalled()
 
         derived.keys?.add('c')
-        c.set(6)
+        stores.c.set(6)
 
         expect(fn).toHaveBeenLastCalledWith({ a: 4, b: 5, c: 6 })
       })
@@ -135,41 +127,33 @@ describe('store', () => {
 
     describe('with proxy key activation', () => {
       test('adapts if set of keys changes because of proxy access', () => {
-        const a = new Writable(1)
-        const b = new Writable(2)
-        const c = new Writable(3)
-
         const fn = vi.fn()
 
-        const derived = new RecordDerived({ a, b, c }, new Set())
+        const { derived, stores } = createDerived(new Set())
 
         derived.subscribe(fn)
 
         fn.mockReset()
 
-        a.set(4)
+        stores.a.set(4)
 
         expect(fn).not.toHaveBeenCalled()
 
-        b.set(5)
+        stores.b.set(5)
 
         expect(fn).not.toHaveBeenCalled()
 
         derived.proxy.c
-        c.set(6)
+        stores.c.set(6)
 
         expect(fn).toHaveBeenLastCalledWith({ a: 4, b: 5, c: 6 })
       })
     })
 
     test('removes all subscriptions when unsubscribed', () => {
-      const a = new Writable(1)
-      const b = new Writable(2)
-      const c = new Writable(3)
-
       const fn = vi.fn()
 
-      const derived = new RecordDerived({ a, b, c })
+      const { derived, stores } = createDerived()
 
       const unsubscribe = derived.subscribe(fn)
 
@@ -177,22 +161,16 @@ describe('store', () => {
 
       unsubscribe()
 
-      a.set(4)
-      b.set(5)
-      c.set(6)
+      stores.a.set(4)
+      stores.b.set(5)
+      stores.c.set(6)
 
       expect(fn).not.toHaveBeenCalled()
     })
 
     describe('notify', () => {
       test('does not notify when pending', () => {
-        const stores = {
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        }
-
-        const derived = new RecordDerived(stores)
+        const { derived } = createDerived()
 
         derived.subscribe(async () => {
           await new Promise((resolve) => setTimeout(resolve, 500))
@@ -212,13 +190,7 @@ describe('store', () => {
       })
 
       test('adds keysChangedDuringFrozen to keys when frozen', () => {
-        const stores = {
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        }
-
-        const derived = new RecordDerived(stores)
+        const { derived } = createDerived()
 
         derived.subscribe(() => {})
 
@@ -232,13 +204,7 @@ describe('store', () => {
 
     describe('transaction', () => {
       test('does not notify if no relevant keys changed during transaction', () => {
-        const stores = {
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        }
-
-        const derived = new RecordDerived(stores, new Set())
+        const { derived, stores } = createDerived(new Set())
 
         const fn = vi.fn()
 
@@ -254,13 +220,7 @@ describe('store', () => {
       })
 
       test('notifies if relevant keys changed during transaction', () => {
-        const stores = {
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        }
-
-        const derived = new RecordDerived(stores, new Set())
+        const { derived, stores } = createDerived(new Set())
 
         // Subscribe to the 'a' writable store.
         derived.proxy.a
@@ -279,13 +239,7 @@ describe('store', () => {
 
     describe('freeze', () => {
       test('does not notify while frozen', () => {
-        const stores = {
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        }
-
-        const derived = new RecordDerived(stores, new Set())
+        const { derived, stores } = createDerived(new Set())
 
         derived.freeze()
 
@@ -303,13 +257,7 @@ describe('store', () => {
       })
 
       test('notifies with correct values immediately after unfreezing', () => {
-        const stores = {
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        }
-
-        const derived = new RecordDerived(stores)
+        const { derived, stores } = createDerived()
 
         derived.freeze()
 
@@ -343,11 +291,7 @@ describe('store', () => {
 
     describe('notify', () => {
       test('does not notify if context is false', () => {
-        const derived = new RecordDerived({
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        })
+        const { derived } = createDerived()
 
         const fn = vi.fn()
 
@@ -361,14 +305,7 @@ describe('store', () => {
       })
 
       test('notifies for exact specific tracked name', () => {
-        const derived = new RecordDerived(
-          {
-            a: new Writable(1),
-            b: new Writable(2),
-            c: new Writable(3),
-          },
-          new Set(),
-        )
+        const { derived } = createDerived(new Set())
 
         derived.keyNames.a = ['Aponia', 'Elysia']
 
@@ -388,14 +325,7 @@ describe('store', () => {
       })
 
       test('notifies for loose specific tracked name', () => {
-        const derived = new RecordDerived(
-          {
-            a: new Writable(1),
-            b: new Writable(2),
-            c: new Writable(3),
-          },
-          new Set(),
-        )
+        const { derived } = createDerived(new Set())
 
         derived.keyNames.a = ['Aponia', 'Elysia']
 
@@ -417,13 +347,7 @@ describe('store', () => {
 
     describe('freeze', () => {
       test('does not notify if pending', () => {
-        const stores = {
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        }
-
-        const derived = new RecordDerived(stores)
+        const { derived, stores } = createDerived()
 
         const fn = vi.fn()
 
@@ -443,13 +367,7 @@ describe('store', () => {
       })
 
       test('notifies if no filter keys specified', () => {
-        const stores = {
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        }
-
-        const derived = new RecordDerived(stores)
+        const { derived, stores } = createDerived()
 
         const fn = vi.fn()
 
@@ -467,13 +385,7 @@ describe('store', () => {
       })
 
       test('notifies for specified filtered keys', () => {
-        const stores = {
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        }
-
-        const derived = new RecordDerived(stores, new Set(['a']))
+        const { derived, stores } = createDerived(new Set(['a']))
 
         const fn = vi.fn()
 
@@ -491,13 +403,7 @@ describe('store', () => {
       })
 
       test('does not notify for specified filtered keys', () => {
-        const stores = {
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        }
-
-        const derived = new RecordDerived(stores, new Set(['b']))
+        const { derived, stores } = createDerived(new Set(['b']))
 
         const fn = vi.fn()
 
@@ -515,13 +421,7 @@ describe('store', () => {
       })
 
       test('notifies when context force is true', () => {
-        const stores = {
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        }
-
-        const derived = new RecordDerived(stores, new Set(['b']))
+        const { derived, stores } = createDerived(new Set(['b']))
 
         const fn = vi.fn()
 
@@ -539,13 +439,7 @@ describe('store', () => {
       })
 
       test('does not notify when context force is false', () => {
-        const stores = {
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        }
-
-        const derived = new RecordDerived(stores, new Set(['b']))
+        const { derived, stores } = createDerived(new Set(['b']))
 
         const fn = vi.fn()
 
@@ -563,13 +457,7 @@ describe('store', () => {
       })
 
       test('notifies when context strings are subset of tracked', () => {
-        const stores = {
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        }
-
-        const derived = new RecordDerived(stores, new Set(['b']))
+        const { derived, stores } = createDerived(new Set(['b']))
 
         const fn = vi.fn()
 
@@ -591,11 +479,7 @@ describe('store', () => {
 
     describe('track', () => {
       test('tracks key names array', () => {
-        const derived = new RecordDerived({
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        })
+        const { derived } = createDerived()
 
         derived.track('a', ['a', 'b', 'c'])
 
@@ -603,11 +487,7 @@ describe('store', () => {
       })
 
       test('tracks key names', () => {
-        const derived = new RecordDerived({
-          a: new Writable(1),
-          b: new Writable(2),
-          c: new Writable(3),
-        })
+        const { derived } = createDerived()
 
         derived.track('a', 'abc')
 
