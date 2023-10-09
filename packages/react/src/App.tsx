f7d00b833b1928b1b0bc956c8d9a35a17b406d06
Aponia <brianmv@uci.edu>
2023-10-09 16:27:39 -0700

    feat: ✨ basic mvp of use-form-state

--- packages/react/src/App.tsx
+++ packages/react/src/App.tsx
@@ -1,13 +1,42 @@
-import { useEffect } from 'react'
+import type { FormControl } from '@forms.js/core'
+import { useCallback, useEffect, useRef, useSyncExternalStore } from 'react'
 
 import { useForm } from './use-form'
 // import { useForm } from 'react-hook-form'
 
+export type UseFormStateProps = {
+  name: string | string[]
+  control: FormControl<any>
+}
+
+export function useFormState(props: UseFormStateProps) {
+  const proxy = useRef(props.control.derivedState.createTrackingProxy(props.name))
+
+  const subscribe = useCallback(
+    (callback: () => void) => {
+      return props.control.derivedState.subscribe(() => {
+        callback()
+      })
+    },
+    [props.control],
+  )
+
+  const getSnapshot = useCallback(() => {
+    return proxy.current
+  }, [])
+
+  useSyncExternalStore(subscribe, getSnapshot)
+
+  return proxy.current
+}
+
 export function App() {
-  const { register, setError, watch } = useForm<{
+  const { register, setError, formControl } = useForm<{
     test: string
     t1: string
   }>()
+
+  const formState = useFormState({ name: ['test', 't1'], control: formControl })
 
   useEffect(() => {
     // setError('test', {
@@ -16,8 +45,7 @@ export function App() {
     // })
   }, [setError])
 
-  console.log(watch('t1'))
-  console.log('render')
+  console.log('render', formState.values)
 
   return (
     <div>
