2d2d9c4764f7f6cef4abd247b7a48c1f83bf897d
Aponia <brianmv@uci.edu>
2023-10-12 14:12:27 -0700

    test: 🚨 use-form-context

--- packages/react/src/form-provider.tsx
+++ packages/react/src/form-provider.tsx
@@ -1,10 +1,9 @@
-import type { FormControlState } from '@forms.js/core'
 import { createContext, useCallback, useSyncExternalStore } from 'react'
 
 import type { ReactFormControl } from './form-control'
 
 export type FormControlContextValue<
-  TValues extends Record<string, any> = Record<string, any>,
+  TValues extends Record<string, any>,
   TContext = any,
   TTransformedValues extends Record<string, any> | undefined = undefined,
   TControl extends ReactFormControl<TValues, TContext, TTransformedValues> = ReactFormControl<
@@ -14,21 +13,30 @@ export type FormControlContextValue<
   >,
 > = {
   control: TControl
-  formState: FormControlState<TValues>
+  formState: TControl['state']
 } & {
   setValue: TControl['setValue']
   register: TControl['registerReact']
+  handleSubmit: TControl['handleSubmitReact']
 }
 
-export const FormControlContext = createContext<FormControlContextValue>(undefined!)
+export const FormControlContext = createContext<FormControlContextValue<any>>(undefined!)
 
 export type FormControlProviderProps<
   TValues extends Record<string, any>,
   TContext = any,
   TTransformedValues extends Record<string, any> | undefined = undefined,
+  TControl extends ReactFormControl<TValues, TContext, TTransformedValues> = ReactFormControl<
+    TValues,
+    TContext,
+    TTransformedValues
+  >,
 > = {
   children?: React.ReactNode
-  control: ReactFormControl<TValues, TContext, TTransformedValues>
+  control: TControl
+} & {
+  setValue?: TControl['setValue']
+  register?: TControl['registerReact']
 }
 
 export function FormControlProvider<
@@ -53,13 +61,17 @@ export function FormControlProvider<
 
   useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot)
 
+  const { control, ...rest } = props as any
+
   return (
     <FormControlContext.Provider
       value={{
-        control: props.control as any,
-        formState: props.control.derivedState.proxy as any,
-        setValue: props.control.setValue.bind(props.control),
-        register: props.control.registerReact.bind(props.control),
+        control,
+        formState: control.derivedState.proxy,
+        setValue: control.setValue.bind(props.control),
+        register: control.registerReact.bind(props.control),
+        handleSubmit: control.handleSubmitReact.bind(props.control),
+        ...rest,
       }}
     >
       {props.children}
---
+++ packages/react/tests/use-form-context.test.tsx
@@ -0,0 +1,222 @@
+import { deepEqual } from '@forms.js/core/utils/deep-equal'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import React from 'react'
+import { describe, it, expect, vi as jest } from 'vitest'
+
+import { FormProvider } from '../src/form-provider'
+import { useController } from '../src/use-controller'
+import { useForm } from '../src/use-form'
+import { useFormContext } from '../src/use-form-context'
+import { useFormState } from '../src/use-form-state'
+import { useWatch } from '../src/use-watch'
+
+describe('FormProvider', () => {
+  it('should have access to all methods with useFormContext', () => {
+    const mockRegister = jest.fn()
+    const Test = () => {
+      const { register } = useFormContext()
+
+      React.useEffect(() => {
+        register('test')
+      }, [register])
+
+      return null
+    }
+
+    const App = () => {
+      const methods = useForm()
+
+      return (
+        <FormProvider {...methods} register={mockRegister}>
+          <form>
+            <Test />
+          </form>
+        </FormProvider>
+      )
+    }
+
+    render(<App />)
+
+    expect(mockRegister).toHaveBeenCalled()
+  })
+
+  it('should work correctly with Controller, useWatch, useFormState.', async () => {
+    const TestComponent = () => {
+      const { field } = useController({
+        name: 'test',
+        defaultValue: '',
+      })
+      return <input {...field} />
+    }
+
+    const TestWatch = () => {
+      const value = useWatch({
+        name: 'test',
+      })
+
+      return <p>Value: {value === undefined ? 'undefined value' : value}</p>
+    }
+
+    const TestFormState = () => {
+      const { isDirty } = useFormState()
+
+      return <div>Dirty: {isDirty ? 'yes' : 'no'}</div>
+    }
+
+    const Component = () => {
+      const methods = useForm()
+      return (
+        <FormProvider {...methods}>
+          <TestComponent />
+          <TestWatch />
+          <TestFormState />
+        </FormProvider>
+      )
+    }
+
+    render(<Component />)
+
+    const input = screen.getByRole('textbox')
+
+    expect(input).toBeVisible()
+    expect(await screen.findByText('Value: undefined value')).toBeVisible()
+    expect(screen.getByText('Dirty: no')).toBeVisible()
+
+    fireEvent.change(input, { target: { value: 'test' } })
+    expect(screen.getByText('Value: test')).toBeVisible()
+    expect(screen.getByText('Dirty: yes')).toBeVisible()
+  })
+
+  it('should not throw type error', () => {
+    type FormValues = {
+      firstName: string
+    }
+
+    type Context = {
+      someValue: boolean
+    }
+
+    function App() {
+      const methods = useForm<FormValues, Context>()
+      const { handleSubmit, register } = methods
+
+      return (
+        <div>
+          <FormProvider {...methods}>
+            <form onSubmit={handleSubmit(() => {})}>
+              <input {...register('firstName')} placeholder="First Name" />
+              <input type="submit" />
+            </form>
+          </FormProvider>
+        </div>
+      )
+    }
+
+    render(<App />)
+  })
+
+  it('should be able to access defaultValues within formState', () => {
+    type FormValues = {
+      firstName: string
+      lastName: string
+    }
+
+    const defaultValues = {
+      firstName: 'a',
+      lastName: 'b',
+    }
+
+    const Test1 = () => {
+      const methods = useFormState()
+
+      return <p>{deepEqual(methods.defaultValues, defaultValues) ? 'context-yes' : 'context-no'}</p>
+    }
+
+    const Test = () => {
+      const methods = useFormContext()
+
+      return <p>{deepEqual(methods.formState.defaultValues, defaultValues) ? 'yes' : 'no'}</p>
+    }
+
+    const Component = () => {
+      const methods = useForm<FormValues>({
+        defaultValues,
+      })
+
+      return (
+        <FormProvider {...methods}>
+          <Test />
+          <Test1 />
+          <button
+            onClick={() => {
+              methods.reset({
+                firstName: 'c',
+                lastName: 'd',
+              })
+            }}
+          >
+            reset
+          </button>
+          <p>{JSON.stringify(defaultValues)}</p>
+        </FormProvider>
+      )
+    }
+
+    render(<Component />)
+
+    expect(screen.getByText('yes')).toBeVisible()
+    expect(screen.getByText('context-yes')).toBeVisible()
+
+    screen.getByText(JSON.stringify(defaultValues))
+
+    fireEvent.click(screen.getByRole('button'))
+
+    waitFor(() => {
+      expect(screen.getByText('yes')).not.toBeValid()
+      expect(screen.getByText('context-yes')).not.toBeVisible()
+
+      screen.getByText(
+        JSON.stringify({
+          firstName: 'c',
+          lastName: 'd',
+        }),
+      )
+    })
+  })
+
+  it('should report errors correctly', async () => {
+    const Child = () => {
+      const {
+        formState: { errors },
+        register,
+        handleSubmit,
+      } = useFormContext<{
+        test: string
+      }>()
+
+      return (
+        <form onSubmit={handleSubmit(() => {})}>
+          <input {...register('test', { required: 'This is required' })} />
+          <p>{errors.test?.message}</p>
+          <button>submit</button>
+        </form>
+      )
+    }
+
+    const App = () => {
+      const methods = useForm()
+
+      return (
+        <FormProvider {...methods}>
+          <Child />
+        </FormProvider>
+      )
+    }
+
+    render(<App />)
+
+    fireEvent.click(screen.getByRole('button'))
+
+    await waitFor(() => screen.getByText('This is required'))
+  })
+})
