433bbe65182919d68cec8824c8ec93929fea16d0
Aponia <brianmv@uci.edu>
2023-10-13 11:37:45 -0700

    feat: ✨ almost done with  some methods

--- packages/core/src/field-array.ts
+++ packages/core/src/field-array.ts
@@ -1,6 +1,11 @@
+import { Writable } from '@forms.js/common/store'
+
 import { FormControl } from './form-control'
 import type { RegisterOptions } from './types/register'
 import type { Validate } from './types/validation'
+import { deepSet } from './utils/deep-set'
+import { isObject } from './utils/is-object'
+import { safeGet } from './utils/safe-get'
 import type { NestedObjectArrays } from './utils/types/nested-object-arrays'
 
 export type FieldArrayOptions<
@@ -20,13 +25,40 @@ export type FieldArrayOptions<
   } & Pick<RegisterOptions<TValues>, 'maxLength' | 'minLength' | 'required'>
 }
 
+/**
+ * Field array focus options.
+ */
+export type FieldArrayMethodProps = {
+  /**
+   * Whether to toggle focus on and off.
+   */
+  shouldFocus?: boolean
+
+  /**
+   * Set focus by either field index.
+   */
+  focusIndex?: number
+
+  /**
+   * Set focus by field name.
+   */
+  focusName?: string
+}
+
 export class FieldArray<
   TValues extends Record<string, any>,
   TContext = any,
   TTransformedValues extends Record<string, any> | undefined = undefined,
   TFieldArray extends NestedObjectArrays<TValues> = NestedObjectArrays<TValues>,
-  TFieldArrayName extends keyof TFieldArray = keyof TFieldArray,
+  TFieldArrayName extends Extract<keyof TFieldArray, string> = Extract<keyof TFieldArray, string>,
+  TFieldArrayValue extends TFieldArray[TFieldArrayName] = TFieldArray[TFieldArrayName],
 > {
+  ids: string[] = []
+
+  fields = new Writable<TFieldArrayValue>()
+
+  focus?: string
+
   constructor(
     public options: FieldArrayOptions<
       TValues,
@@ -35,10 +67,116 @@ export class FieldArray<
       TFieldArray,
       TFieldArrayName
     >,
+    public name = options.name,
     public control = options.control,
   ) {}
 
-  append() {}
+  getControlFieldArrayValues(): TFieldArrayValue {
+    const controlFieldArrayValues =
+      safeGet(
+        this.control.state.status.value.mount
+          ? this.control.state.values.value
+          : this.control.state.defaultValues.value,
+        this.options.name,
+      ) ?? this.control.options.shouldUnregister
+        ? safeGet(this.control.state.defaultValues.value, this.options.name) ?? []
+        : []
+
+    return controlFieldArrayValues as TFieldArrayValue
+  }
+
+  /**
+   * Update state in the form control.
+   */
+  updateFormControl(
+    mutateArray: (args: any[]) => any[],
+    shouldSetValues = true,
+    shouldUpdateFieldsAndState = true,
+  ) {
+    // _state.action = true
+
+    const field = safeGet(this.control.fields, this.name)
+
+    if (shouldUpdateFieldsAndState && Array.isArray(field)) {
+      const fieldValues = mutateArray(field)
+
+      if (shouldSetValues) {
+        deepSet(this.control.fields, this.name, fieldValues)
+      }
+    }
+
+    const errors = safeGet(this.control.state.errors.value, this.name)
+
+    if (shouldUpdateFieldsAndState && Array.isArray(errors)) {
+      const newErrors = mutateArray(errors)
+
+      this.control.state.errors.update((currentErrors) => {
+        if (shouldSetValues) {
+          deepSet(currentErrors, this.name, newErrors)
+        }
+
+        unsetEmptyArray(currentErrors, this.name)
+
+        return currentErrors
+      })
+    }
+
+    const touchedFields = safeGet(this.control.state.touchedFields.value, this.name)
+
+    if (
+      _proxyFormState.touchedFields &&
+      shouldUpdateFieldsAndState &&
+      Array.isArray(touchedFields)
+    ) {
+      const newTouchedFields = mutateArray(touchedFields)
+
+      if (shouldSetValues) {
+        this.control.state.touchedFields.update((currentTouchedFields) => {
+          deepSet(currentTouchedFields, this.name, newTouchedFields)
+          return currentTouchedFields
+        })
+      }
+    }
+
+    if (_proxyFormState.dirtyFields) {
+      _formState.dirtyFields = getDirtyFields(_defaultValues, _formValues)
+    }
+
+    // _subjects.state.next({
+    //   name,
+    //   isDirty: _getDirty(name, values),
+    //   dirtyFields: _formState.dirtyFields,
+    //   errors: _formState.errors,
+    //   isValid: _formState.isValid,
+    // })
+  }
+
+  append(
+    value: Partial<TFieldArrayValue[number]> | Partial<TFieldArrayValue>,
+    options?: FieldArrayMethodProps,
+  ) {
+    const valueClone = structuredClone(value)
+
+    const valuesArray = (Array.isArray(valueClone) ? valueClone : [valueClone]).filter(Boolean)
+
+    const updatedFieldArrayValues = [...this.getControlFieldArrayValues(), ...valuesArray]
+
+    this.ids.push(...valuesArray.map(generateId))
+
+    this.focus = getFocusFieldName(this.name, updatedFieldArrayValues.length - 1, options)
+
+    this.control.state.values.update((currentValues) => {
+      deepSet(currentValues, this.name, updatedFieldArrayValues)
+      return currentValues
+    })
+
+    this.fields.set(updatedFieldArrayValues as any)
+
+    this.updateFormControl((args) => {
+      args.push(...valuesArray.map(() => undefined))
+      return args
+    })
+  }
 
   prepend() {}
 
@@ -55,15 +193,62 @@ export class FieldArray<
   replace() {}
 }
 
-// export type MyForm = {
-//   a: string
-//   b: {
-//     c: number
-//   }
-//   d: string[]
-//   e: {
-//     f: {
-//       g: boolean
-//     }[]
-//   }[]
-// }
+function unsetEmptyArray<T>(ref: T, name: string) {
+  if (!safeGet<any[]>(ref, name).filter(Boolean).length) {
+    unset(ref, name)
+  }
+}
+
+function unset<T>(obj: T, path?: string, defaultValue?: unknown): any {
+  if (!path || !isObject(obj)) {
+    return defaultValue
+  }
+
+  const result = path
+    .split(/[,[\].]+?/)
+    .filter(Boolean)
+    .reduce((result, key) => (result == null ? result : result[key as keyof {}]), obj)
+
+  return result == null || result === obj
+    ? obj[path as keyof T] == null
+      ? defaultValue
+      : obj[path as keyof T]
+    : result
+}
+
+function getFocusFieldName(
+  name: string,
+  index: number,
+  options: FieldArrayMethodProps = {},
+): string {
+  return options.shouldFocus || options.shouldFocus == null
+    ? options.focusName || `${name}.${options.focusIndex == null ? index : options.focusIndex}.`
+    : ''
+}
+
+function generateId() {
+  const number = typeof performance === 'undefined' ? Date.now() : performance.now() * 1000
+
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16 + number) % 16 | 0
+
+    return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16)
+  })
+}
+
+export type MyForm = {
+  a: string
+  b: {
+    c: number
+  }
+  d: string[]
+  e: {
+    f: {
+      g: boolean
+    }[]
+  }[]
+}
+
+const control = new FormControl<MyForm>()
+
+export const fieldArray = new FieldArray({ name: 'e', control })
