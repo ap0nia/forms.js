2171b2129f67ee8c7019e92874f39e311e2ce76f
Aponia <brianmv@uci.edu>
2023-10-17 10:10:58 -0700

    fix: 🐛 build step and entry point

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,11 @@
 export * from './form-control'
 export * from './constants'
+
+export * from './types/errors'
+export * from './types/form'
+export * from './types/field-array'
+export * from './types/fields'
+export * from './types/html'
+export * from './types/register'
+export * from './types/resolver'
+export * from './types/validation'
