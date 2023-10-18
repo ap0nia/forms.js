1f67eceec2874f036ba0fe97e58d1a610dcb3edd
Aponia <brianmv@uci.edu>
2023-10-17 17:48:38 -0700

    test: 🚨 constructor

--- packages/core/tests/form-control/constructor.test.ts
+++ packages/core/tests/form-control/constructor.test.ts
@@ -4,60 +4,112 @@ import { FormControl } from '../../src/form-control'
 
 describe('FormControl', () => {
   describe('constructor', () => {
-    test('no default values and no values sets both as empty object', () => {
-      const form = new FormControl()
+    describe('correctly sets default values and values', () => {
+      test('sets default values as empty if not provided', () => {
+        const form = new FormControl()
 
-      expect(form.state.defaultValues.value).toEqual({})
-
-      expect(form.state.values.value).toEqual({})
-    })
-
-    test('default values and no values sets both', () => {
-      const defaultValues = { foo: 'bar' }
-
-      const form = new FormControl({ defaultValues })
-
-      expect(form.state.defaultValues.value).toEqual(defaultValues)
-
-      expect(form.state.values.value).toEqual(defaultValues)
-    })
-
-    test('default values as function and no values sets both', () => {
-      const defaultValues = () => ({ foo: 'bar' })
-
-      const form = new FormControl({ defaultValues })
-
-      expect(form.state.defaultValues.value).toEqual(defaultValues())
-
-      expect(form.state.values.value).toEqual(defaultValues())
-    })
-
-    test('values and no default values sets both', () => {
-      const values = { foo: 'bar' }
-
-      const form = new FormControl({ values })
-
-      expect(form.state.defaultValues.value).toEqual(values)
-
-      expect(form.state.values.value).toEqual(values)
-    })
-
-    test('shouldUnregister and default values does not set values', () => {
-      const defaultValues = { foo: 'bar' }
-
-      const form = new FormControl({ defaultValues, shouldUnregister: true })
-
-      expect(form.state.defaultValues.value).toEqual(defaultValues)
-
-      expect(form.state.values.value).toEqual({})
-    })
-
-    test('shouldCaptureDirtyFields is true if specified by resetOptions', () => {
-      const form = new FormControl({
-        resetOptions: { keepDirtyValues: true },
+        expect(form.state.values.value).toEqual({})
+        expect(form.state.defaultValues.value).toEqual({})
       })
 
-      expect(form.options.shouldCaptureDirtyFields).toBeTruthy()
+      test('sets default values as provided object', () => {
+        const defaultValues = {
+          a: 1,
+          b: 2,
+          c: 3,
+        }
+
+        const form = new FormControl({ defaultValues })
+
+        expect(form.state.values.value).toEqual(defaultValues)
+        expect(form.state.defaultValues.value).toEqual(defaultValues)
+      })
+
+      test('sets default values as result of provided function', () => {
+        const defaultValues = {
+          a: 1,
+          b: 2,
+          c: 3,
+        }
+
+        const form = new FormControl({ defaultValues: () => defaultValues })
+
+        expect(form.state.values.value).toEqual(defaultValues)
+        expect(form.state.defaultValues.value).toEqual(defaultValues)
+      })
+
+      test('sets values as provided values', () => {
+        const values = {
+          a: 1,
+          b: 2,
+          c: 3,
+        }
+
+        const form = new FormControl({ values })
+
+        expect(form.state.values.value).toEqual(values)
+        expect(form.state.defaultValues.value).toEqual(values)
+      })
+
+      test('sets values to empty object if shouldUnregister is true', () => {
+        const values = {
+          a: 1,
+          b: 2,
+          c: 3,
+        }
+
+        const form = new FormControl({ shouldUnregister: true, values })
+
+        expect(form.state.values.value).toEqual({})
+        expect(form.state.defaultValues.value).toEqual(values)
+      })
+    })
+
+    describe('correctly sets initial state', () => {
+      test('sets isLoading to true if default values is a promise', () => {
+        const defaultValues = Promise.resolve({
+          a: 1,
+          b: 2,
+          c: 3,
+        })
+
+        const form = new FormControl({ defaultValues })
+
+        expect(form.state.isLoading.value).toBeTruthy()
+      })
+
+      test('sets submitCount to 0', () => {
+        const form = new FormControl()
+        expect(form.state.submitCount.value).toEqual(0)
+      })
+
+      test('sets all the boolean state to false', () => {
+        const form = new FormControl()
+
+        expect(form.state.isDirty.value).toBeFalsy()
+        expect(form.state.isValidating.value).toBeFalsy()
+        expect(form.state.isSubmitted.value).toBeFalsy()
+        expect(form.state.isSubmitting.value).toBeFalsy()
+        expect(form.state.isSubmitSuccessful.value).toBeFalsy()
+        expect(form.state.isValidating.value).toBeFalsy()
+        expect(form.state.isValid.value).toBeFalsy()
+      })
+
+      test('sets all the object state to empty objects', () => {
+        const form = new FormControl()
+
+        expect(form.state.errors.value).toEqual({})
+        expect(form.state.touchedFields.value).toEqual({})
+        expect(form.state.dirtyFields.value).toEqual({})
+      })
+    })
+
+    describe('correctly sets options', () => {
+      test('sets shouldCaptureDirtyFields to provided value of resetOptions.keepDirtyValues', () => {
+        const form = new FormControl({ resetOptions: { keepDirtyValues: true } })
+
+        expect(form.options.shouldCaptureDirtyFields).toBeTruthy()
+      })
     })
   })
 })
