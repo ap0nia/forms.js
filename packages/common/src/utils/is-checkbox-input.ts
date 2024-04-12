02e9b6ac3c08fb53cf6714aee206c2142baf688c
Aponia <brianmv@uci.edu>
2024-04-12 16:16:48 -0700

    refactor(common): ♻️ dedicated types file

--- packages/common/src/utils/is-checkbox-input.ts
+++ packages/common/src/utils/is-checkbox-input.ts
@@ -1,40 +1,4 @@
-import type { Noop } from './noop'
-
-declare const $NestedValue: unique symbol
-
-/**
- * @deprecated to be removed in the next major version
- */
-export type NestedValue<TValue extends object = object> = {
-  [$NestedValue]: never
-} & TValue
-
-export type IsFlatObject<T extends object> = Extract<
-  Exclude<T[keyof T], NestedValue | Date | FileList>,
-  any[] | object
-> extends never
-  ? true
-  : false
-
-export type FieldName<TFieldValues extends Record<string, any>> =
-  IsFlatObject<TFieldValues> extends true ? Extract<keyof TFieldValues, string> : string
-
-export type CustomElement<TFieldValues extends Record<string, any>> = Partial<HTMLElement> & {
-  name: FieldName<TFieldValues>
-  type?: string
-  value?: any
-  disabled?: boolean
-  checked?: boolean
-  options?: HTMLOptionsCollection
-  files?: FileList | null
-  focus?: Noop
-}
-
-export type FieldElement<TFieldValues extends Record<string, any> = Record<string, any>> =
-  | HTMLInputElement
-  | HTMLSelectElement
-  | HTMLTextAreaElement
-  | CustomElement<TFieldValues>
+import type { FieldElement } from '../types'
 
 export function isCheckboxInput(element: FieldElement): element is HTMLInputElement {
   return element.type === 'checkbox'
