962ad7a22bcf84940eea0d64511b88df2f6ac429
Aponia <brianmv@uci.edu>
2024-04-14 09:17:59 -0700

    test(common): 🚨 is-empty-object util

---
+++ packages/common/__tests__/utils/is-empty-object.test.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect } from 'vitest'
+
+import { isEmptyObject } from '../../src/utils/is-empty-object'
+
+describe('isEmptyObject', () => {
+  it('should return true when value is an empty object', () => {
+    expect(isEmptyObject({})).toBeTruthy()
+  })
+
+  it('should return false when value is not an empty object', () => {
+    expect(isEmptyObject(null)).toBeFalsy()
+    expect(isEmptyObject(undefined)).toBeFalsy()
+    expect(isEmptyObject(-1)).toBeFalsy()
+    expect(isEmptyObject(0)).toBeFalsy()
+    expect(isEmptyObject(1)).toBeFalsy()
+    expect(isEmptyObject('')).toBeFalsy()
+    expect(isEmptyObject(() => null)).toBeFalsy()
+    expect(isEmptyObject({ foo: 'bar' })).toBeFalsy()
+    expect(isEmptyObject([])).toBeFalsy()
+    expect(isEmptyObject(['foo', 'bar'])).toBeFalsy()
+  })
+})
