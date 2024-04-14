132a4b24a0487e379726bbde3e50fbf36a168e67
Aponia <brianmv@uci.edu>
2024-04-14 09:13:03 -0700

    fix(common): 🐛 is-primitive returns true if value is nullish

--- packages/common/src/utils/is-primitive.ts
+++ packages/common/src/utils/is-primitive.ts
@@ -3,5 +3,5 @@ import { isObjectType } from './is-object'
 export type Primitive = null | undefined | string | number | boolean | symbol | bigint
 
 export function isPrimitive(value: unknown): value is Primitive {
-  return value != null || !isObjectType(value)
+  return value == null || !isObjectType(value)
 }
