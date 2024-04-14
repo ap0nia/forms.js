d0eb7b8ab2354aa9ac02adc04ec07428952ce5b5
Aponia <brianmv@uci.edu>
2024-04-14 09:03:37 -0700

    feat(common): ✨ unset util

---
+++ packages/common/src/utils/unset.ts
@@ -0,0 +1,52 @@
+import { isEmptyObject } from './is-empty-object'
+import { isKey } from './is-key'
+import { isObject } from './is-object'
+import { stringToPath } from './string-to-path'
+
+function baseGet(object: any, updatePath: (string | number)[]) {
+  const length = updatePath.slice(0, -1).length
+  let index = 0
+
+  while (index < length) {
+    const key = updatePath[index++]
+
+    if (key != null) {
+      object = object === undefined ? index++ : object[key]
+    }
+  }
+
+  return object
+}
+
+function isEmptyArray(obj: unknown[]) {
+  for (const key in obj) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (obj.hasOwnProperty(key) && obj[key] !== undefined) {
+      return false
+    }
+  }
+  return true
+}
+
+export function unset(object: any, path: string | (string | number)[]) {
+  const paths = Array.isArray(path) ? path : isKey(path) ? [path] : stringToPath(path)
+
+  const childObject = paths.length === 1 ? object : baseGet(object, paths)
+
+  const index = paths.length - 1
+  const key = paths[index]
+
+  if (childObject && key != null) {
+    delete childObject[key]
+  }
+
+  if (
+    index !== 0 &&
+    ((isObject(childObject) && isEmptyObject(childObject)) ||
+      (Array.isArray(childObject) && isEmptyArray(childObject)))
+  ) {
+    unset(object, paths.slice(0, -1))
+  }
+
+  return object
+}
