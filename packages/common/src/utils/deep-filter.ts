3355eca7434a5f8011fec89ea4ab7ba60f8c1c2a
Aponia <brianmv@uci.edu>
2024-04-14 20:56:46 -0700

    feat(common): ✨ deep-filter util

---
+++ packages/common/src/utils/deep-filter.ts
@@ -0,0 +1,25 @@
+import { get } from './get'
+import type { Nullish } from './null'
+import { set } from './set'
+
+export function deepFilter(obj: unknown, key?: PropertyKey | PropertyKey[] | Nullish) {
+  if (key == null || obj == null) {
+    return obj
+  }
+
+  const keyArray = Array.isArray(key) ? key : [key]
+
+  if (keyArray.length === 0) {
+    return obj
+  }
+
+  const result = keyArray.reduce((currentResult, currentKey) => {
+    const value = get(obj, currentKey)
+
+    set(currentResult, currentKey, value)
+
+    return currentResult
+  }, {})
+
+  return result
+}
