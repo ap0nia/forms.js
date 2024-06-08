d42997490018c6a4b09cdbd26c1c3481df0cee5f
Aponia <brianmv@uci.edu>
2024-06-08 13:56:45 -0700

    chore(core): 🔧 add react-hook-form isCheckBoxInput tests

--- packages/core/__tests__/logic/getCheckboxValue.test.ts
+++ packages/core/__tests__/logic/getCheckboxValue.test.ts
@@ -1,12 +1,6 @@
 import { describe, it, expect } from 'vitest'
 
-import { isCheckBoxInput, getCheckBoxValue } from '../../src/logic/html/checkbox'
-
-describe('isCheckBoxInput', () => {
-  it('should return true when type is checkbox', () => {
-    expect(isCheckBoxInput({ name: 'test', type: 'checkbox' })).toBeTruthy()
-  })
-})
+import { getCheckBoxValue } from '../../src/logic/html/checkbox'
 
 describe('getCheckboxValue', () => {
   it('should return default value if not valid or empty options', () => {
