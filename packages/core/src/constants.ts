f3c29efe62f7d9b4d050e0000715812ada19757c
Aponia <brianmv@uci.edu>
2024-06-08 03:15:17 -0700

    feat(core): ✨ constants

---
+++ packages/core/src/constants.ts
@@ -0,0 +1,23 @@
+export const EVENTS = {
+  BLUR: 'blur',
+  FOCUS_OUT: 'focusout',
+  CHANGE: 'change',
+} as const
+
+export const VALIDATION_MODE = {
+  onBlur: 'onBlur',
+  onChange: 'onChange',
+  onSubmit: 'onSubmit',
+  onTouched: 'onTouched',
+  all: 'all',
+} as const
+
+export const INPUT_VALIDATION_RULES = {
+  max: 'max',
+  min: 'min',
+  maxLength: 'maxLength',
+  minLength: 'minLength',
+  pattern: 'pattern',
+  required: 'required',
+  validate: 'validate',
+} as const
