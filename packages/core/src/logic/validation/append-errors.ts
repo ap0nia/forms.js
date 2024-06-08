f7cb42df2d205653898c126edd94b10f772783dd
Aponia <brianmv@uci.edu>
2024-06-08 14:27:52 -0700

    feat(core): ✨ react-hook-form append-errors

--- packages/core/src/logic/validation/append-errors.ts
+++ packages/core/src/logic/validation/append-errors.ts
@@ -2,6 +2,10 @@ import type { FieldErrorRecord } from '../../types/errors'
 import type { ValidateResult } from '../../types/validation'
 
 /**
+ * This function is not currently used in favor of merging errors explictily...
+ *
+ * To be integrated, it should be added to {@type NativeValidationContext} and passed to validation middleware.
+ *
  * @see https://github.com/react-hook-form/react-hook-form/blob/1d0503b46cfe0589b188c4c0d9fa75f247271cf7/src/logic/appendErrors.ts
  */
 export function appendErrors(
@@ -21,3 +25,5 @@ export function appendErrors(
     },
   }
 }
+
+export default appendErrors
