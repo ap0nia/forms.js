60e10836ad440d9ad78a8d6b29f3338ae360d616
Aponia <brianmv@uci.edu>
2024-06-08 03:47:47 -0700

    feat(core): ✨ non-record non-primitives
    Types that are not primitives, but should not be recurred into as
    `Record<string, any`

---
+++ packages/core/src/utils/not-record.ts
@@ -0,0 +1,17 @@
+/**
+ * Special edge cases whenever recurring into records, aside from the explicit "any" type.
+ */
+export type NonRecordNonPrimitives =
+  | any[]
+  | BigInt
+  | Date
+  | Function
+  | Map<any, any>
+  | Set<any>
+  | Symbol
+
+export type NotRecord<T> = T extends Record<string, any>
+  ? T extends NonRecordNonPrimitives
+    ? T
+    : never
+  : T
