f97886ea640a1cffe98de39121968b45d3c865a3
Aponia <brianmv@uci.edu>
2024-06-08 06:53:57 -0700

    feat(core): ✨ is-html-element helper

---
+++ packages/core/src/logic/html/is-html-element.ts
@@ -0,0 +1,14 @@
+import { isBrowser } from '@forms.js/common/utils/is-browser'
+
+/**
+ * @see https://github.com/react-hook-form/react-hook-form/blob/master/src/utils/isHTMLElement.ts
+ */
+export function isHTMLElement(value: unknown): value is HTMLElement {
+  if (!isBrowser) {
+    return false
+  }
+
+  const owner = value ? ((value as HTMLElement).ownerDocument as Document) : 0
+
+  return value instanceof (owner && owner.defaultView ? owner.defaultView.HTMLElement : HTMLElement)
+}
