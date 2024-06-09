8963b8acb4956bf3573c9cc30957bdf1ddbe3f7c
Aponia <brianmv@uci.edu>
2024-06-08 21:46:18 -0700

    feat(core): ✨ form-control constructor

--- packages/core/src/form-control.ts
+++ packages/core/src/form-control.ts
@@ -1,16 +1,16 @@
 import { Batchable, Writable } from '@forms.js/common/store'
-import { deepEqual } from '@forms.js/common/utils/deep-equal'
-import { deepFilter } from '@forms.js/common/utils/deep-filter'
-import { set } from '@forms.js/common/utils/set'
-import { unset } from '@forms.js/common/utils/unset'
-import { isBrowser } from '@forms.js/common/utils/is-browser'
-import { isObject } from '@forms.js/common/utils/is-object'
-import { isEmptyObject } from '@forms.js/common/utils/is-empty-object'
-import { isPrimitive } from '@forms.js/common/utils/is-primitive'
-import type { Nullish } from '@forms.js/common/utils/null'
-import { get, getMultiple } from '@forms.js/common/utils/get'
-import { stringToPath } from '@forms.js/common/utils/string-to-path'
 
+// import { deepEqual } from '@forms.js/common/utils/deep-equal'
+// import { deepFilter } from '@forms.js/common/utils/deep-filter'
+// import { set } from '@forms.js/common/utils/set'
+// import { unset } from '@forms.js/common/utils/unset'
+// import { isBrowser } from '@forms.js/common/utils/is-browser'
+// import { isObject } from '@forms.js/common/utils/is-object'
+// import { isEmptyObject } from '@forms.js/common/utils/is-empty-object'
+// import { isPrimitive } from '@forms.js/common/utils/is-primitive'
+// import type { Nullish } from '@forms.js/common/utils/null'
+// import { get, getMultiple } from '@forms.js/common/utils/get'
+// import { stringToPath } from '@forms.js/common/utils/string-to-path'
 import {
   VALIDATION_EVENTS,
   type CriteriaMode,
@@ -18,34 +18,31 @@ import {
   type SubmissionValidationMode,
   type ValidationEvent,
 } from './constants'
-
-import { lookupError } from './logic/errors/lookup-error'
-import { filterFields } from './logic/fields/filter-fields'
-import { focusFieldBy } from './logic/fields/focus-field-by'
-import { getFieldEventValue } from './logic/fields/get-field-event-value'
-import { getDirtyFields } from './logic/fields/get-dirty-fields'
-import { getFieldValue, getFieldValueAs } from './logic/fields/get-field-value'
-import { hasValidation } from './logic/fields/has-validation'
-import { iterateFieldsByAction } from './logic/fields/iterate-fields-by-action'
-import { updateFieldReference } from './logic/fields/update-field-reference'
-import { elementIsLive } from './logic/html/element-is-live'
-import { isHTMLElement } from './logic/html/is-html-element'
-import { mergeElementWithField } from './logic/html/merge-element-with-field'
+// import { lookupError } from './logic/errors/lookup-error'
+// import { filterFields } from './logic/fields/filter-fields'
+// import { focusFieldBy } from './logic/fields/focus-field-by'
+// import { getFieldEventValue } from './logic/fields/get-field-event-value'
+// import { getDirtyFields } from './logic/fields/get-dirty-fields'
+// import { getFieldValue, getFieldValueAs } from './logic/fields/get-field-value'
+// import { hasValidation } from './logic/fields/has-validation'
+// import { iterateFieldsByAction } from './logic/fields/iterate-fields-by-action'
+// import { updateFieldReference } from './logic/fields/update-field-reference'
+// import { elementIsLive } from './logic/html/element-is-live'
+// import { isHTMLElement } from './logic/html/is-html-element'
+// import { mergeElementWithField } from './logic/html/merge-element-with-field'
 import { getValidationModes } from './logic/validation/get-validation-modes'
-import { nativeValidateFields } from './logic/validation/native-validation'
-import type { NativeValidationResult } from './logic/validation/native-validation/types'
-import { shouldSkipValidationAfter } from './logic/validation/should-skip-validation-after'
-
+// import { nativeValidateFields } from './logic/validation/native-validation'
+// import type { NativeValidationResult } from './logic/validation/native-validation/types'
+// import { shouldSkipValidationAfter } from './logic/validation/should-skip-validation-after'
 import type { FieldErrors } from './types/errors'
-import type { HTMLFieldElement, Field, FieldRecord } from './types/fields'
+import type { /* HTMLFieldElement ,*/ Field, FieldRecord } from './types/fields'
 import type { ParseForm } from './types/parse'
 import type { Resolver } from './types/resolver'
-
 import type { DeepMap } from './utils/deep-map'
 import type { DeepPartial } from './utils/deep-partial'
-import type { Defaults } from './utils/defaults'
-import type { KeysToProperties } from './utils/keys-to-properties'
-import type { LiteralUnion } from './utils/literal-union'
+import type { Defaults, ValueOrDeepPartial } from './utils/defaults'
+// import type { KeysToProperties } from './utils/keys-to-properties'
+// import type { LiteralUnion } from './utils/literal-union'
 
 export type FormControlState<T = Record<string, any>> = {
   isDirty: boolean
@@ -179,4 +176,165 @@ export const defaultFormControlOptions: FormControlOptions = {
    * If an error is found during validation, the first field with an error is focused.
    */
   shouldFocusError: true,
+}
+
+/**
+ * Core API.
+ */
+export class FormControl<
+  TValues extends Record<string, any> = Record<string, any>,
+  TContext = any,
+  // TTransformedValues extends Record<string, any> | undefined = undefined,
+  // TParsedForm extends ParseForm<TValues> = ParseForm<TValues>,
+> {
+  /**
+   * Internally resolved options that control the form control's behavior.
+   */
+  options: ResolvedFormControlOptions<TValues, TContext>
+
+  /**
+   * State represented as a record of writable stores.
+   *
+   * This is not optimized for notifications; it may change multiple times in a function.
+   */
+  stores: { [K in keyof FormControlState<TValues>]: Writable<FormControlState<TValues>[K]> }
+
+  /**
+   * Buffers updates to {@link stores} until it's flushed.
+   *
+   * This is optimized for notifications and generally flushes 1-2 times per function.
+   */
+  state: Batchable<this['stores']>
+
+  /**
+   * Registered fields.
+   */
+  fields: FieldRecord = {}
+
+  /**
+   * Names of fields, describes their current status.
+   */
+  names = {
+    /**
+     * Names of fields that are currently mounted.
+     */
+    mount: new Set<string>(),
+
+    /**
+     * Names of fields that are currently unmounted (should be unregistered).
+     */
+    unMount: new Set<string>(),
+
+    /**
+     * Names of field arrays.
+     */
+    array: new Set<string>(),
+  }
+
+  /**
+   * Callbacks that are invoked specifically when {@link setValue} or {@link reset} is called.
+   */
+  valueListeners: ((newValues: TValues) => unknown)[] = []
+
+  mounted = false
+
+  constructor(options?: FormControlOptions<TValues, TContext>) {
+    const mode = options?.mode ?? defaultFormControlOptions.mode
+    const reValidateMode = options?.reValidateMode ?? defaultFormControlOptions.reValidateMode
+
+    this.options = {
+      mode,
+      reValidateMode,
+      shouldFocusError: defaultFormControlOptions.shouldFocusError,
+      shouldDisplayAllAssociatedErrors: options?.criteriaMode === VALIDATION_EVENTS.all,
+      submissionValidationMode: {
+        beforeSubmission: getValidationModes(mode),
+        afterSubmission: getValidationModes(reValidateMode),
+      },
+      shouldCaptureDirtyFields: Boolean(options?.resetOptions?.keepDirtyValues),
+      ...options,
+    }
+
+    const initialDefaultValues =
+      typeof options?.defaultValues === 'function'
+        ? options.defaultValues()
+        : options?.defaultValues
+
+    const isLoading = initialDefaultValues instanceof Promise
+
+    const defaultValues: any =
+      (!isLoading && structuredClone(initialDefaultValues)) ||
+      structuredClone(options?.values ?? {})
+
+    this.stores = {
+      submitCount: new Writable(0),
+      isDirty: new Writable(false),
+      isLoading: new Writable(isLoading),
+      isValidating: new Writable(false),
+      isSubmitted: new Writable(false),
+      isSubmitting: new Writable(false),
+      isSubmitSuccessful: new Writable(false),
+      isValid: new Writable(false),
+      touchedFields: new Writable({}),
+      dirtyFields: new Writable({}),
+      defaultValues: new Writable(defaultValues),
+      errors: new Writable({}),
+      values: new Writable(options?.shouldUnregister ? {} : structuredClone(defaultValues)),
+      disabled: new Writable(Boolean(options?.disabled)),
+    }
+
+    this.state = new Batchable(this.stores, new Set())
+
+    if (isLoading) {
+      this.resolveDefaultValues(initialDefaultValues, true)
+    }
+  }
+
+  /**
+   * Resolve the form control's default values.
+   *
+   * Maybe sync or async depending on the default values provided.
+   */
+  resolveDefaultValues(defaults?: Defaults<TValues>, resetValues?: boolean) {
+    const resolvingDefaultValues = typeof defaults === 'function' ? defaults() : defaults
+
+    if (resolvingDefaultValues == null) {
+      this.stores.isLoading.set(false)
+      return
+    }
+
+    const isPromise = resolvingDefaultValues instanceof Promise
+
+    this.stores.isLoading.set(isPromise)
+
+    if (isPromise) {
+      return resolvingDefaultValues.then((resolvedDefaultValues = {} as any) => {
+        return this.finalizeResolveDefaultValues(resolvedDefaultValues, resetValues)
+      })
+    }
+
+    const resolvedDefaultValues = resolvingDefaultValues ?? {}
+
+    return this.finalizeResolveDefaultValues(resolvedDefaultValues, resetValues)
+  }
+
+  /**
+   * Always finalize the resolution of default values synchronously.
+   */
+  finalizeResolveDefaultValues(
+    resolvedDefaultValues: ValueOrDeepPartial<TValues>,
+    resetValues?: boolean,
+  ) {
+    this.state.open()
+
+    this.stores.defaultValues.set(resolvedDefaultValues as any)
+
+    if (resetValues) {
+      this.stores.values.set(structuredClone(resolvedDefaultValues) as TValues)
+    }
+
+    this.stores.isLoading.set(false)
+
+    this.state.flush()
+  }
 }
