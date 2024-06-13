49c28e4f18d7de29e332787d8e010f02fd16d858
Aponia <brianmv@uci.edu>
2024-06-13 07:44:29 -0700

    test(react): 🚨 controller

---
+++ packages/react/__tests__/controller.server.tsx
@@ -0,0 +1,28 @@
+import { renderToString } from 'react-dom/server'
+
+import { Controller } from '../src/controller'
+import { useForm } from '../src/use-form'
+
+describe('Controller with SSR', () => {
+  /**
+   * @see https://github.com/react-hook-form/react-hook-form/issues/1398
+   */
+  it('should render correctly with as with component', () => {
+    const Component = () => {
+      const { control } = useForm<{
+        test: string
+      }>()
+
+      return (
+        <Controller
+          defaultValue="default"
+          name="test"
+          render={({ field }) => <input {...field} />}
+          control={control}
+        />
+      )
+    }
+
+    renderToString(<Component />)
+  })
+})
